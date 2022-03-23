37964623fe889cff480e74cdb85c3039e141b9b3
efrem.mengesha <efrem.mengesha@flant.com>
2022-03-23 23:39:52 +0300

    fix

--- start.sh
+++ start.sh
@@ -18,14 +18,14 @@ fi
 
 mv docker-compose.yaml docker-compose-`date +%Y%m%d`-`date +%H%M%S`.yaml
 
-if [ "$ROLE" = "server" ]; then
+if [ "$ROLE" = "client" ]; then
     echo "Generating a docker-compose.yaml for the $ROLE ..."
     for FILE in $(ls 10-files/*.tpl); do
         echo "collect $FILE ..."
         sed -e 's/$ROLE/'"$ROLE"'/g' -e 's/$SERVER/'"$SERVER"'/g' -e 's/$CLIENT/'"$CLIENT"'/g' $FILE >> docker-compose.yaml
     done
     echo -e "Done, now you can just: docker-compose up -d"
-elif [ "$ROLE" = "client" ]; then
+elif [ "$ROLE" = "server" ]; then
     echo "Generating a docker-compose.yaml for the $ROLE ..."
     sed -e 's/$ROLE/'"$ROLE"'/g' -e 's/$SERVER/'"$SERVER"'/g' -e 's/$CLIENT/'"$CLIENT"'/g' 10-files/00-goben.tpl >> docker-compose.yaml
     echo -e "Done, now you can just: docker-compose up -d"
