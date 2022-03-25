65d471e63ba6510a3dc6a899647e87c2d7132bf5
efrem.mengesha <efrem.mengesha@flant.com>
2022-03-25 12:20:07 +0300

    log

--- 00-scripts/bandwidth.sh
+++ 00-scripts/bandwidth.sh
@@ -10,48 +10,67 @@
 
 INTERVAL="${INTERVAL:-60}"
 GOBEN_OUTPUT_FILE="/tmp/report.txt"
+GOBEN_PORT="9045"
 
 METRICS_NAME="goben_speed_test"
 METRICS_PATH="$(echo $METRICS_DIR/index.html)"
 METRICS_LABELS="reading writing"
 
-if [ -z "$ROLE" ]; then
-    echo "Variable \$ROLE is not set"
-    exit 1
-fi
+function log {
+    timestamp=$(date "+%Y-%m-%d %H:%M:%S")
+    message="$1"
+    role="$ROLE"
+    echo '{}' | \
+    jq  --monochrome-output \
+        --compact-output \
+        --raw-output \
+        --arg timestamp "$timestamp" \
+        --arg role "$role" \
+        --arg message "$message" \
+        '.timestamp=$timestamp|.role=$role|.message=$message'
+}
 
 function exporter {
     for LABEL in $METRICS_LABELS; do
         CHECK_NAME="$(echo $METRICS_NAME{label=$LABEL, client_node=$CLIENT, server_node=$SERVER})"
         CHECK_RESULT_MB=$(cat $GOBEN_OUTPUT_FILE | grep aggregate | grep $LABEL | awk '{print $5}')
-        grep -w $CHECK_NAME $METRICS_PATH > /dev/null
+        log "$CHECK_NAME $CHECK_RESULT_MB"
+        grep -w "$CHECK_NAME" $METRICS_PATH > /dev/null
         if [[ $? -eq 0 ]] ; then
-            sed -i "s/$METRICS_NAME .*/$METRICS_NAME $CHECK_RESULT_MB/g" $METRICS_PATH
+            sed -i "s/$CHECK_NAME .*/$CHECK_NAME $CHECK_RESULT_MB/g" $METRICS_PATH
         else
-            echo "$METRICS_NAME $CHECK_RESULT_MB" >> $METRICS_PATH
+            echo "$CHECK_NAME $CHECK_RESULT_MB" >> $METRICS_PATH
         fi
     done
 }
 
 function goben_server {
-    $GOBEN_BIN -defaultPort ":9045"
+    log "Starting up the goben server on port $GOBEN_PORT"
+    $GOBEN_BIN -defaultPort ":$GOBEN_PORT"
 }
 
 function goben_client {
+    log "Starting up the goben client to test the speed up to $SERVER:$GOBEN_PORT with interval $INTERVAL"
     while true; do
-        $GOBEN_BIN -hosts $SERVER:9045 -ascii=false -tls=false -reportInterval 2s -totalDuration 10s &> $GOBEN_OUTPUT_FILE;
+        $GOBEN_BIN -hosts $SERVER:$GOBEN_PORT -ascii=false -tls=false -reportInterval 2s -totalDuration 10s &> $GOBEN_OUTPUT_FILE;
         sleep $INTERVAL;
         exporter
     done
 }
 
+if [ -z "$ROLE" ]; then
+    log "Variable \$ROLE is not set"
+    exit 1
+fi
+
 if [ "$ROLE" = "server" ]; then
-    echo "Run goben in server mode ..."
+    log "Run goben in server mode"
     goben_server
 elif [ "$ROLE" = "client" ]; then
-    echo "Run goben in client mode ..."
+    log "Run goben in client mode"
     goben_client
 else
-    echo "Check the \$ROLE variable. Can only be a \"server\" or a \"client\""
+    e
+    log "Check the \$ROLE variable. Can only be a \"server\" or a \"client\""
     exit 1
 fi
