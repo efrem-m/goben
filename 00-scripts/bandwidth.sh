abd046f3aa5ec6fc53a08c1c78ec606f5302181b
efrem.mengesha <efrem.mengesha@flant.com>
2022-03-25 21:40:06 +0300

    fix

--- 00-scripts/bandwidth.sh
+++ 00-scripts/bandwidth.sh
@@ -47,7 +47,7 @@ function exporter {
 
 function iperf3_server {
     log "Starting up the iperf3 server on port $PORT"
-    iperf3 --server --port $PORT --interval 2 --json &> /dev/null
+    iperf3 --server --port $PORT --interval 2 --json &> /dev/null || log "error - $?"
 }
 
 function iperf3_client {
